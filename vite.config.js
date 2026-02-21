525f01cf2572bcb3774697aee5bc76097e067502
ARTEM KOTOSHCHUK <skabullartem@gmail.com>
2026-02-21 18:11:03 +0700

    Update .gitignore and config files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  server: { port: 5173 },
+  server: {
+    port: 5173,
+    open: '/codenames/' // автоматично відкривати index.html
+  },
   base: '/codenames/',
   build: {
     rollupOptions: {
